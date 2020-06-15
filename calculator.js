d36886db9b4e66f41a9eb40315f30a2ab80f114e
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-06-15 20:05:39 +0300

    simple calculator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 // Define the numbers and the operation here
-
+let num1, num2, operation;
+num1 = 3;
+num2 = 3;
+operation = "q";
 // Write if-statements here
+if (operation === "add") {
+  console.log(num1 + num2);
+} else if (operation === "subtract") {
+  console.log(num1 - num2);
+} else if (operation === "multiply") {
+  console.log(num1 * num2);
+} else if (operation === "divide") {
+  console.log(num1 / num2);
+} else {
+  console.log("Error! you should use an arithematic operation!");
+}
